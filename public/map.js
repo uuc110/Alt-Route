859389425fb17829ae65492444df5a1e1d704403
Sourabh Kushwah <uuc110@gmail.com>
2024-03-18 23:46:48 +0530

    Reupload Repo

---
+++ public/map.js
@@ -0,0 +1,473 @@
+var ar = [
+    {
+        "lat": 22.75334,
+        "lng": 75.89347000000001
+    },
+    {
+        "lat": 22.752180000000003,
+        "lng": 75.89312000000001
+    },
+    {
+        "lat": 22.751820000000002,
+        "lng": 75.89301
+    },
+    {
+        "lat": 22.751910000000002,
+        "lng": 75.89259000000001
+    },
+    {
+        "lat": 22.752080000000003,
+        "lng": 75.89178000000001
+    },
+    {
+        "lat": 22.75234,
+        "lng": 75.89063
+    },
+    {
+        "lat": 22.752260000000003,
+        "lng": 75.8906
+    },
+    {
+        "lat": 22.75225,
+        "lng": 75.89063
+    },
+    {
+        "lat": 22.75224,
+        "lng": 75.89065000000001
+    },
+    {
+        "lat": 22.75222,
+        "lng": 75.89068
+    },
+    {
+        "lat": 22.75213,
+        "lng": 75.89072
+    },
+    {
+        "lat": 22.75205,
+        "lng": 75.89071000000001
+    },
+    {
+        "lat": 22.751990000000003,
+        "lng": 75.89065000000001
+    },
+    {
+        "lat": 22.751970000000004,
+        "lng": 75.89058
+    },
+    {
+        "lat": 22.751980000000003,
+        "lng": 75.89049
+    },
+    {
+        "lat": 22.75202,
+        "lng": 75.89044000000001
+    },
+    {
+        "lat": 22.75205,
+        "lng": 75.89041
+    },
+    {
+        "lat": 22.752070000000003,
+        "lng": 75.8904
+    },
+    {
+        "lat": 22.752100000000002,
+        "lng": 75.89031
+    },
+    {
+        "lat": 22.75225,
+        "lng": 75.88962000000001
+    },
+    {
+        "lat": 22.75277,
+        "lng": 75.88758
+    },
+    {
+        "lat": 22.752910000000004,
+        "lng": 75.88684
+    },
+    {
+        "lat": 22.75318,
+        "lng": 75.88572
+    },
+    {
+        "lat": 22.75334,
+        "lng": 75.88493000000001
+    },
+    {
+        "lat": 22.75345,
+        "lng": 75.88422
+    },
+    {
+        "lat": 22.75375,
+        "lng": 75.88306
+    },
+    {
+        "lat": 22.75391,
+        "lng": 75.88239
+    },
+    {
+        "lat": 22.754140000000003,
+        "lng": 75.88099000000001
+    },
+    {
+        "lat": 22.7544,
+        "lng": 75.87986000000001
+    },
+    {
+        "lat": 22.754610000000003,
+        "lng": 75.87905
+    },
+    {
+        "lat": 22.75475,
+        "lng": 75.87859
+    },
+    {
+        "lat": 22.754790000000003,
+        "lng": 75.87855
+    },
+    {
+        "lat": 22.75495,
+        "lng": 75.87852000000001
+    },
+    {
+        "lat": 22.75496,
+        "lng": 75.87849
+    },
+    {
+        "lat": 22.75497,
+        "lng": 75.87845
+    },
+    {
+        "lat": 22.755000000000003,
+        "lng": 75.87841
+    },
+    {
+        "lat": 22.755070000000003,
+        "lng": 75.87837
+    },
+    {
+        "lat": 22.755090000000003,
+        "lng": 75.87836
+    },
+    {
+        "lat": 22.755070000000003,
+        "lng": 75.87791
+    },
+    {
+        "lat": 22.755080000000003,
+        "lng": 75.87759000000001
+    },
+    {
+        "lat": 22.755070000000003,
+        "lng": 75.8769
+    },
+    {
+        "lat": 22.75506,
+        "lng": 75.87608
+    },
+    {
+        "lat": 22.755020000000002,
+        "lng": 75.87536
+    },
+    {
+        "lat": 22.75497,
+        "lng": 75.87488
+    },
+    {
+        "lat": 22.75496,
+        "lng": 75.87465
+    },
+    {
+        "lat": 22.75495,
+        "lng": 75.87394
+    },
+    {
+        "lat": 22.75494,
+        "lng": 75.87243000000001
+    },
+    {
+        "lat": 22.75494,
+        "lng": 75.87208000000001
+    },
+    {
+        "lat": 22.754980000000003,
+        "lng": 75.87166
+    },
+    {
+        "lat": 22.755200000000002,
+        "lng": 75.86998000000001
+    },
+    {
+        "lat": 22.754230000000003,
+        "lng": 75.86987
+    },
+    {
+        "lat": 22.752390000000002,
+        "lng": 75.86963
+    },
+    {
+        "lat": 22.752380000000002,
+        "lng": 75.86965000000001
+    },
+    {
+        "lat": 22.752360000000003,
+        "lng": 75.86967
+    },
+    {
+        "lat": 22.75231,
+        "lng": 75.86968
+    },
+    {
+        "lat": 22.752270000000003,
+        "lng": 75.86966000000001
+    },
+    {
+        "lat": 22.75224,
+        "lng": 75.86962000000001
+    },
+    {
+        "lat": 22.75224,
+        "lng": 75.86961000000001
+    },
+    {
+        "lat": 22.752160000000003,
+        "lng": 75.86959
+    },
+    {
+        "lat": 22.7509,
+        "lng": 75.86947
+    },
+    {
+        "lat": 22.750290000000003,
+        "lng": 75.86941
+    },
+    {
+        "lat": 22.74961,
+        "lng": 75.86930000000001
+    },
+    {
+        "lat": 22.748630000000002,
+        "lng": 75.86921000000001
+    },
+    {
+        "lat": 22.748410000000003,
+        "lng": 75.8692
+    },
+    {
+        "lat": 22.747880000000002,
+        "lng": 75.86912000000001
+    },
+    {
+        "lat": 22.747780000000002,
+        "lng": 75.86969
+    },
+    {
+        "lat": 22.747490000000003,
+        "lng": 75.87047000000001
+    },
+    {
+        "lat": 22.74699,
+        "lng": 75.87164
+    },
+    {
+        "lat": 22.74668,
+        "lng": 75.87229
+    },
+    {
+        "lat": 22.746570000000002,
+        "lng": 75.8725
+    },
+    {
+        "lat": 22.746470000000002,
+        "lng": 75.87245
+    },
+    {
+        "lat": 22.74639,
+        "lng": 75.87244000000001
+    },
+    {
+        "lat": 22.746350000000003,
+        "lng": 75.87242
+    },
+    {
+        "lat": 22.74596,
+        "lng": 75.87209
+    },
+    {
+        "lat": 22.745250000000002,
+        "lng": 75.87181000000001
+    },
+    {
+        "lat": 22.745240000000003,
+        "lng": 75.87182
+    },
+    {
+        "lat": 22.745220000000003,
+        "lng": 75.87183
+    },
+    {
+        "lat": 22.74517,
+        "lng": 75.87184
+    },
+    {
+        "lat": 22.745130000000003,
+        "lng": 75.87182
+    },
+    {
+        "lat": 22.7451,
+        "lng": 75.87177000000001
+    },
+    {
+        "lat": 22.7451,
+        "lng": 75.87172000000001
+    },
+    {
+        "lat": 22.744960000000003,
+        "lng": 75.87166
+    }
+]
+var map;
+var directionsService;
+var directionsRenderer;
+
+function initMap() {
+    directionsService = new google.maps.DirectionsService();
+    directionsRenderer = new google.maps.DirectionsRenderer();
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 37.7749, lng: -122.4194},
+        zoom: 13
+    });
+
+    directionsRenderer.setMap(map);
+
+    var sourceInput = document.getElementById('source');
+    var destInput = document.getElementById('dest');
+
+    var sourceAutocomplete = new google.maps.places.Autocomplete(sourceInput);
+    var destAutocomplete = new google.maps.places.Autocomplete(destInput);
+
+    google.maps.event.addListener(sourceAutocomplete, 'place_changed', function() {
+        var place = sourceAutocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+    });
+
+    google.maps.event.addListener(destAutocomplete, 'place_changed', function() {
+        var place = destAutocomplete.getPlace();
+        if (!place.geometry) {
+            window.alert("No details available for input: '" + place.name + "'");
+            return;
+        }
+    });
+}
+
+function calcRoute() {
+    var source = document.getElementById('source').value;
+    var dest = document.getElementById('dest').value;
+
+    var request = {
+        origin: source,
+        destination: dest,
+        travelMode: 'DRIVING',
+        provideRouteAlternatives: true
+    };
+
+    directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+            directionsRenderer.setDirections({ routes: [] });
+
+            result.routes.sort(function(a, b) {
+                return a.legs[0].distance.value - b.legs[0].distance.value;
+            });
+
+            var routeCoordinates = [];
+
+            for (var i = 0; i < result.routes.length; i++) {
+                var routeColor = '#ADD8E6';
+
+                var rendererOptions = {
+                    map: map,
+                    directions: result,
+                    routeIndex: i,
+                    polylineOptions: {
+                        strokeColor: routeColor,
+                        strokeWeight: 4,
+                        strokeOpacity: 0.8
+                    }
+                };
+                var renderer = new google.maps.DirectionsRenderer(rendererOptions);
+
+                var path = result.routes[i].overview_path;
+                var coords = [];
+                for (var j = 0; j < path.length; j++) {
+                    coords.push({ lat: path[j].lat(), lng: path[j].lng() });
+                }
+                routeCoordinates.push(coords);
+
+                if (compareCoordinates(coords, ar)) {
+                    renderer.setOptions({
+                        polylineOptions: {
+                            strokeColor: '#8B0000'
+                        }
+                    });
+                }
+            }
+
+            console.log(routeCoordinates);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+}
+
+function plotShortestPath() {
+    var source = document.getElementById('source').value;
+    var dest = document.getElementById('dest').value;
+    
+    var request = {
+        origin: source,
+        destination: dest,
+        travelMode: 'DRIVING'
+    };
+
+    directionsService.route(request, function(result, status) {
+        if (status == 'OK') {
+            directionsRenderer.setDirections({ routes: [] });
+
+            var rendererOptions = {
+                map: map,
+                directions: result,
+                polylineOptions: {
+                    strokeColor: '#006400', // Dark green color for shortest route
+                    strokeWeight: 6,
+                    strokeOpacity: 0.8
+                }
+            };
+            var renderer = new google.maps.DirectionsRenderer(rendererOptions);
+            renderer.setMap(map);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+}
+
+function compareCoordinates(route, ar) {
+    if (route.length !== ar.length) {
+        return false;
+    }
+
+    for (var i = 0; i < route.length; i++) {
+        if (route[i].lat !== ar[i].lat || route[i].lng !== ar[i].lng) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
